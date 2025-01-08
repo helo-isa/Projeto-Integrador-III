ea18d1f527af148442c32c54876298c629f2c44c
Kauan <kauanlucastoldo@gmail.com>
2025-01-08 14:40:37 -0300

    Menu mobile

---
+++ app/static/js2/index.js
@@ -0,0 +1,7 @@
+document.querySelector('#btn-open-menu').addEventListener('click', () => {
+    document.querySelector('#container-mobile').style.display = 'flex';
+})
+
+document.querySelector('#btn-close-menu').addEventListener('click', () => {
+    document.querySelector('#container-mobile').style.display = 'none';
+})
